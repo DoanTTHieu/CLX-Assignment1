0524c7171ba396d1b23ce52940d0d07feafa4548
Thanh Hieu <18520734@gm.uit.edu.vn>
2020-03-19 01:04:54 +0700

    CoffeeShop_Menu_isLogedIn

--- react-native-basic/CoffeeShop/component/Main/Drawer.js
+++ react-native-basic/CoffeeShop/component/Main/Drawer.js
@@ -1,37 +1,65 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { createDrawerNavigator, DrawerContentScrollView,
-  DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { View, StyleSheet, Image, TouchableOpacity, Text } from 'react-native';
+import {
+  createDrawerNavigator, DrawerContentScrollView,
+  DrawerItemList } from '@react-navigation/drawer';
 import OdersHistory from '../Menu/OdersHistory';
 import ChangeInfo from '../Menu/ChangeInfo';
 import account from '../../assets/Others/account.png';
 import TabNavigator from './TabNavigator';
 
-const Drawer = createDrawerNavigator();
+class CustomDrawerContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      islogedin: true,
+    };
+  }
 
-function CustomDrawerContent(props) {
-  return (
-    <View style={styles.customStyle}>
-      <View>
-        <Image source={account} style={{ width: 100, height: 100 }} />
+  render() {
+    const { container, profile, btnStyle, btnText } = styles;
+    const logoutJSX = (
+      <View style={{ flex: 1 }}>
+        <TouchableOpacity style={btnStyle}>
+          <Text style={btnText}>Sign in</Text>
+        </TouchableOpacity>
       </View>
-      <View>
-        <DrawerContentScrollView {...props}>
-          <DrawerItemList {...props} />
-          <DrawerItem label="Sign out" />
+    );
+    const loginJSX = (
+      <View style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }} >
+        <Text style={{ color: '#203546', fontFamily: 'Avenir' }}>USERNAME</Text>
+        <DrawerContentScrollView {...this.props}>
+          <DrawerItemList {...this.props} labelStyle={{ fontSize: 18 }} />
+          {/* <DrawerItem label="Sign out" labelStyle={styles.label} /> */}
         </DrawerContentScrollView>
       </View>
-    </View>
-  );
+    );
+
+    const mainJSX = this.state.islogedin ? loginJSX : logoutJSX;
+    return (
+      <View style={container}>
+        <Image source={account} style={profile} />
+        {mainJSX}
+      </View>
+    );
+  }
 }
 
+{/* <DrawerContentScrollView {...props}>
+<DrawerItemList {...props} labelStyle={{ fontSize: 18 }} />
+{/* <DrawerItem label="Sign out" labelStyle={styles.label} /> */}
+// </DrawerContentScrollView> */}
+
+const Drawer = createDrawerNavigator();
+
+// eslint-disable-next-line react/no-multi-comp
 export default class MenuDrawer extends Component {
   render() {
     return (
       <View style={{ flex: 1 }} >
 
         <Drawer.Navigator
-          drawerContent={props => <CustomDrawerContent {...props} />}
+          drawerContent={(props) => <CustomDrawerContent {...props} />}
           initialRouteName="Home"
           drawerStyle={{
             backgroundColor: '#f7c744',
@@ -51,6 +79,29 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     flex: 1,
     alignItems: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#f7c744',
+    alignItems: 'center',
     paddingTop: 100
+  },
+  profile: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 5
+  },
+  btnStyle: {
+    height: 50,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    paddingHorizontal: 70
+  },
+  btnText: {
+    color: '#f7c744',
+    fontSize: 20
   }
 });
